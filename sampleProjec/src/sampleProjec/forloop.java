380388fa9bb0db95a8f93140696699cb6cce32ea
maruthikp <kmaruthichowdary@gmail.com>
2019-07-22 22:30:48 +1000

    uploading trying

---
+++ sampleProjec/src/sampleProjec/forloop.java
@@ -0,0 +1,17 @@
+package sampleProjec;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		for(i=0;i<3;i++) {System.out.println(" ");
+			for(j=0;j<i;j++) {
+				System.out.println("*");
+				
+			}
+		}
+
+	}
+
+}
