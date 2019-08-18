b828c9a93a4d886e9e0e12f6b2d85885b0ad7498
maruthikp <kmaruthichowdary@gmail.com>
2019-08-18 17:47:08 +1000

    TestNG,Values in dropdownlist,

--- ChromeAuto/src/main/java/sampleProject/ChromeAuto/SeleniumLocaters.java
+++ ChromeAuto/src/main/java/sampleProject/ChromeAuto/SeleniumLocaters.java
@@ -9,14 +9,17 @@ public class SeleniumLocaters {
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","D:\\Eclipse\\git\\ChromeAuto\\src\\test\\resources\\drivers\\chromedriver.exe");
 		WebDriver dr = new ChromeDriver();
-		dr.get("http://monsterindia.com");Thread.sleep(2000);
+		/*
+		 * dr.get("http://monsterindia.com");Thread.sleep(2000);
+		 * 
+		 * //dr.findElement(By.name("fts")).sendKeys("selenium");
+		 * dr.findElement(By.className("btn")).click();Thread.sleep(2000);
+		 * 
+		 * dr.findElement(By.xpath("//input[@class='btn btn-orange uprcse']")).click();
+		 */	
 		
-		dr.findElement(By.name("fts")).sendKeys("selenium");
-		dr.findElement(By.className("btn")).click();
-		
-		
-		
-		
+		//dr.get("http://amazon.com.au");
+	//		dr.findElement(By.xpath("//option[@te))
 		
 		
 		Thread.sleep(2000);
--- ChromeAuto/src/test/java/sampleTestNGPractice/FirstTestNGcls.java
+++ ChromeAuto/src/test/java/sampleTestNGPractice/FirstTestNGcls.java
@@ -1,15 +1,57 @@
 package sampleTestNGPractice;
 
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+
 public class FirstTestNGcls {
+	WebDriver driver=null;
 	
-	
-	@Test
-	public void Testcase02()
+	@BeforeSuite
+	public void hello() {
+		
+		System.setProperty("webdriver.chrome.driver","D:\\Eclipse\\git\\ChromeAuto\\src\\test\\resources\\drivers\\chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.get("http:\\monsterindia.com");
+		}
+	@BeforeMethod
+	public void helloMethod()
 	{
-		System.out.println("hello");
+		//System.out.println("hello");
+		driver.findElement(By.xpath("//select/option[text()='Vietnam']")).click();
+	}
+	@Test
+	public void stest()
+	{
+		//System.out.println("hello");
+		driver.findElement(By.xpath("//select/option[text()='India']")).click();
+		
 	}
 
-	
+
+@AfterMethod
+public void SwithWindow() throws InterruptedException
+{
+	String Parent = driver.getWindowHandle();
+	Set<String> windows= driver.getWindowHandles();
+	for(String eachwindow :windows) {
+		driver.switchTo().window(eachwindow);
+		Thread.sleep(2000);
+		
+	}
+}
+
+@AfterSuite
+public void As1()
+{
+	driver.quit();
+}
 }
